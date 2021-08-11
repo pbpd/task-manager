dcc4e4111aa163735b306c931187ab8d84fa47e3
Priyabrat Priyadarshi <priyabrat.me@gmail.com>
2021-08-11 08:41:23 +0530

    commit

--- models/Task.js
+++ models/Task.js
@@ -5,7 +5,7 @@ const TaskSchema = new mongoose.Schema({
     type: String,
     required: [true, 'must provide name'],
     trim: true,
-    maxlength: [20, 'name can not be more than 20 characters'],
+    maxlength: [200, 'name can not be more than 200 characters'],
   },
   completed: {
     type: Boolean,

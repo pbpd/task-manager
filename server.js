7d241b7924ce59194a9fd864214ded532039bae2
Priyabrat Priyadarshi <priyabrat.me@gmail.com>
2021-08-11 07:54:43 +0530

    commit

--- server.js
+++ server.js
@@ -2,27 +2,28 @@ const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
-require('dotenv').config();
+const dotenv = require('dotenv');
 const notFound = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-// middleware
+// load ebv vars
+dotenv.config();
 
+// middleware
 app.use(express.static('./public'));
 app.use(express.json());
 
 // routes
-
 app.use('/api/v1/tasks', tasks);
 
 app.use(notFound);
 app.use(errorHandlerMiddleware);
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI || uri);
-    app.listen(port, console.log(`Server is listening on port ${port}...`));
+    await connectDB();
+    app.listen(PORT, console.log(`Server is listening on port ${PORT}...`));
   } catch (error) {
     console.log(error);
   }
